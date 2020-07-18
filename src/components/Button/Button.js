5875da1ba0cd5f18151d5e4a087e58086f46ee15
“Atessadai” <Atessa.dailami@gmail.com>
2020-07-18 15:27:50 -0400

    buttons

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import './Button.css'
 
 // This is a functional component - just sent up a little differently as an arrow function!
-const Button = (props) => (
-    <button>
-        {props.label}
-    </button>
-)
+const Button = (props) => {
+    let classList = [];
+
+    let types = ['primary', 'danger', 'success', 'warning', 'default', 'icontext', 'icon']
+
+    if (types.includes(props.type)) {
+        classList.push(`button-${props.type}`)
+    }
+
+    if (props.large) {
+        classList.push(`button-${props.type}-large`);
+    }
+
+    if (props.outline) {
+        classList.push(`button-${props.type}-outline`);
+    }
+
+    if (props.faded) {
+        classList.push(`button-${props.type}-faded`);
+    }
+
+    if (props.cart) {
+        classList.push(`button-${props.type}-cart`);
+    }
+
+    if (props.heart) {
+        classList.push(`button-${props.type}-heart`);
+    }
+
+
+    return <button className={classList.join(" ")}>{props.label}</button>
+}
 
 export default Button; 
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,5 +3,45 @@ import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
 
+
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Primary', () => <Button type="primary" label="Do Something" />)
+    .add('Outline Primary', () => <Button type="primary" outline label="Do Something" />)
+    .add('Faded Primary', () => <Button type="primary" faded label="Do Something" />)
+    .add('Large Primary', () => <Button type="primary" large label="Do Something" />)
+    .add('Outline Large Primary', () => <Button type="primary" large outline label="Do Something" />)
+    .add('Faded Large Primary', () => <Button type="primary" large faded label="Do Something" />)
+    .add('Danger', () => <Button type="danger" label="Do Something" />)
+    .add('Outline Danger', () => <Button type="danger" outline label="Do Something" />)
+    .add('Faded Danger', () => <Button type="danger" faded label="Do Something" />)
+    .add('Large Danger', () => <Button type="danger" large label="Do Something" />)
+    .add('Outline Large Danger', () => <Button type="danger" large outline label="Do Something" />)
+    .add('Faded Large Danger', () => <Button type="danger" large faded label="Do Something" />)
+    .add('Success', () => <Button type="success" label="Do Something" />)
+    .add('Outline Success', () => <Button type="success" outline label="Do Something" />)
+    .add('Faded Success', () => <Button type="success" faded label="Do Something" />)
+    .add('Large Success', () => <Button type="success" large label="Do Something" />)
+    .add('Outline Large Success', () => <Button type="success" large outline label="Do Something" />)
+    .add('Faded Large Success', () => <Button type="success" large faded label="Do Something" />)
+    .add('Warning', () => <Button type="warning" label="Do Something" />)
+    .add('Outline Warning', () => <Button type="warning" outline label="Do Something" />)
+    .add('Faded Warning', () => <Button type="warning" faded label="Do Something" />)
+    .add('Large Warning', () => <Button type="warning" large label="Do Something" />)
+    .add('Outline Large Warning', () => <Button type="warning" large outline label="Do Something" />)
+    .add('Faded Large Warning', () => <Button type="warning" large faded label="Do Something" />)
+    .add('Default', () => <Button type="default" label="Do Something" />)
+    .add('Outline Default', () => <Button type="default" outline label="Do Something" />)
+    .add('Faded Default', () => <Button type="default" faded label="Do Something" />)
+    .add('Large Default', () => <Button type="default" large label="Do Something" />)
+    .add('Outline Large Default', () => <Button type="default" large outline label="Do Something" />)
+    .add('Faded Large Default', () => <Button type="default" large faded label="Do Something" />)
+    .add('Cart Icon Text', () => <Button type="icontext" cart label="Add To Cart" />)
+    .add('Heart Icon Text', () => <Button type="icontext" heart label="Add To Favorties" />)
+    .add('Heart Icon', () => <Button type="icon" heart label="" />)
+    .add('Cart Icon', () => <Button type="icon" cart label="" />)
+
+
+
+
+
+
